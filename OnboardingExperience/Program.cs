022efa49b5fae49d115b316f09a6d402deb2b82a
privateporkers <dccotton@firemail.cc>
2018-04-11 10:22:46 -0500

    Implemented a QuestionAnswer() method

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -8,7 +8,15 @@ namespace OnboardingExperience
         {
             Console.WriteLine("Hello and Welcome to Privateporkers National Bank");
 
-            Console.ReadLine();
+            QuestionAnswer("What is your First name?", "Your first name is: ");
+
+        }
+
+        public static void QuestionAnswer(string question, string response)
+        {
+           Console.WriteLine(question);
+          var read = Console.ReadLine();
+          Console.WriteLine(response + read);
         }
     }
 }
