d582e95b41a25f201ff7bdd06dc9c41ed473d6ae
privateporkers <dccotton@firemail.cc>
2018-04-12 12:55:02 -0500

    Removed age WriteLine  and added it to first and last name WriteLine

--- OnboardingExperience/Program.cs
+++ OnboardingExperience/Program.cs
@@ -39,11 +39,9 @@ namespace OnboardingExperience
             //asks the user for there first name then there last. Out putting both first and last name
             user.FirstName = AskQuestion("What is your first name?");
             user.LastName = AskQuestion("what is your last name?");
-            Console.WriteLine($"You name is: {user.FirstName} {user.LastName}");
-            Console.Clear();
-
             user.age = IntQuestion("What is your age?");
-             Console.WriteLine($"Your age is: {user.age}");
+
+            Console.WriteLine($"You name is: {user.FirstName} {user.LastName} and your age is: {user.age}");
         }
         public static string AskQuestion(string question)
         {
